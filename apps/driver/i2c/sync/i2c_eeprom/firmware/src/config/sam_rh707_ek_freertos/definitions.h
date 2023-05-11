f812e2656ec9a4b501d22e0431eec37bb4567e7d
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-05-11 18:08:22 +0530

    [DRV_I2C] Regenerated demo for driver_i2c_sync_i2c_eeprom_sam_rh707_ek_freertos
    MH3-76973

--- apps/driver/i2c/sync/i2c_eeprom/firmware/src/config/sam_rh707_ek_freertos/definitions.h
+++ apps/driver/i2c/sync/i2c_eeprom/firmware/src/config/sam_rh707_ek_freertos/definitions.h
@@ -52,15 +52,15 @@
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/matrix/plib_matrix.h"
 #include "peripheral/nvic/plib_nvic.h"
+#include "bsp/bsp.h"
 #include "FreeRTOS.h"
 #include "task.h"
-#include "bsp/bsp.h"
 #include "driver/i2c/drv_i2c.h"
-#include "peripheral/flexcom/twi/master/plib_flexcom2_twi_master.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
+#include "peripheral/flexcom/twi/master/plib_flexcom2_twi_master.h"
 #include "app_i2c_eeprom.h"
 
 
@@ -72,6 +72,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "ATSAMRH707F18A"
+#define DEVICE_ARCH			 "CORTEX-M7"
+#define DEVICE_FAMILY		 "SAMRH"
+#define DEVICE_SERIES		 "SAMRH707"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 50000000
